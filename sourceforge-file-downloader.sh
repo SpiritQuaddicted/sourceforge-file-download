55e5bdee30f5652a64e0ec8bf0a8973912f1b765
Spirit <spirit@quaddicted.com>
2011-10-29 09:47:18 +0200

    first commit

---
+++ sourceforge-file-downloader.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+project=$1
+echo "Downloading $project's files"
+
+# download all the pages on which direct download links are
+# be nice, sleep a second
+wget -w 1 -np -m -A download http://sourceforge.net/projects/$project/files/
+
+# extract those links
+grep -Rh direct-download sourceforge.net/ | grep -Eo '".*" ' | sed 's/"//g' > urllist
+
+# remove temporary files, unless you want to keep them for some reason
+rm -r sourceforge.net/
+
+# download each of the extracted URLs, put into $projectname/
+while read url; do wget --content-disposition -x -nH --cut-dirs=1 "${url}"; done < urllist
+
+rm urllist
